ec9e78bbaade3feadf1876621dbf09820f9ab978
JB-Tellez <jb.tellez@gmail.com>
2021-04-17 15:55:17 -0700

    first pass at Python + Phaser game

---
+++ index.py
@@ -0,0 +1,35 @@
+from browser import window
+import javascript
+
+Phaser = window.Phaser
+
+class Game(object):
+    def __init__(self):
+
+        config = {
+            'type': Phaser.AUTO,
+            'width': 800,
+            'height': 600,
+            'backgroundColor': '#2d2d2d',
+            'scene': {
+                'preload': self.preload,
+                'create': self.create,
+            }
+        };
+
+        self.game = window.Phaser.Game.new(config)
+
+    def preload(self, *args):
+        this = javascript.this()
+
+        this.load.setBaseURL('http://labs.phaser.io')
+        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
+
+
+    def create(self, *args):
+        this = javascript.this()
+        this.add.image(400, 300, 'logo')
+
+
+
+GAME = Game()
